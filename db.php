27a880a4da201b8325403d525c56a039ea51f544
cushionfall <sapkotam174@gmail.com>
2025-05-19 18:05:56 +0545

    inital commit

---
+++ db.php
@@ -0,0 +1,43 @@
+<?php
+$username="localhost";
+$password="";
+$db="login"
+CREATE TABLE IF NOT EXISTS users{
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(64) NOT NULL UNIQUE,
+    f_name VARCHAR(64) NOT NULL,
+    l_name VARCHAR(64) NOT NULL,
+    bio TEXT,
+    profile_img VARCHAR(124) DEFAULT 'default.png',
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(124) NOT NULL UNIQUE,
+    dob DATETIME NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+};
+CREATE TABLE IF NOT EXISTS session{
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    is_active BOOLEAN DEFAULT TRUE,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    ON DELETE CASCADE
+};
+CREATE TABLE IF NOT EXISTS post{
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    image_post VARCHAR(255),
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+};
+CREATE TABLE IF NOT EXISTS likes{
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INT NOT NULL,
+    post_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES post(id) ON DELETE CASCADE,
+    UNIQUE(user_id, post_id)
+};
+?>
